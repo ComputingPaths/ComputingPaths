7b163825621adffe85f030a47b5a3c79a3a30dc4
Sui Gao <sugao@ucsd.edu>
2017-07-29 01:00:04 -0700

    basic navbar and parts of the home page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('home')
 })
 
 app.get('/sample', (req, res) => {
