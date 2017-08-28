6ed920bd4187ec04a9263798c5bd9c458d1aefca
Sui Gao <Mark_sVi@outlook.com>
2017-08-28 12:05:51 -0700

    remove firebase

--- dev.js
+++ dev.js
@@ -1,27 +1,25 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const { Processor } = require('./render')
+const { Processor } = require('./processor')
 
-const processor = new Processor(() => {
-  fs.watch('views', { recursive: true }, (event, filename) => {
-    if (path.extname(filename) === '.pug') {
-      processor.renderFile(filename)
-      console.log(`${filename} re-rendered`)
-    }
-  })
+let processor = new Processor()
 
-  fs.watch('template.pug', (event, filename) => {
-    processor.renderAll()
-  })
+processor.renderAll()
 
-  fs.watch('content.json', (event, filename) => {
-    fs.readFile('content.json', 'utf8', (err, text) => {
-      if (err) throw err
-      processor.mergeLocals(JSON.parse(text))
-      processor.renderAll()
-    })
-  }) 
+fs.watch('views', { recursive: true }, (event, filename) => {
+  if (path.extname(filename) === '.pug') {
+    processor.renderFile(filename)
+    console.log(`${filename} re-rendered`)
+  }
+})
+
+fs.watch('template.pug', (event, filename) => {
+  processor.renderAll()
+})
+
+fs.watch('content.json', (event, filename) => {
+  processor.renderAll()
 })
 
 const app = express()
