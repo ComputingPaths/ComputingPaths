f59acb3ad35a188ed5c01b3b814c2e160d6f7b25
Caris Wei <cariswei@gmail.com>
2017-08-31 23:08:11 -0700

    Got swiping working on projects modals

--- scripts/projects.js
+++ scripts/projects.js
@@ -44,4 +44,20 @@ $(document).ready(function() {
   var num = index + 1;
   $(value).attr("href","#proj-carousel" + num);
   });
+  
+  $(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(".carousel").carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(".carousel").carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
 })
