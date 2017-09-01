78bfa9573b3603a221ec8a23ecff34493ddf2325
Caris Wei <cariswei@gmail.com>
2017-09-01 14:50:12 -0700

    Nevermind swipe on projects is donezo. Made the controls visible for mobile devices again

--- scripts/projects.js
+++ scripts/projects.js
@@ -47,19 +47,19 @@ $(document).ready(function() {
   
   
   //source: https://stackoverflow.com/questions/21349984/how-to-make-bootstrap-carousel-slider-use-mobile-left-right-swipe
-  $(".this").on("touchstart", function(event){
-        var xClick = event.originalEvent.touches[0].pageX;
-    $(this).one("touchmove", function(event){
-        var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
-            $(".carousel").carousel('next');
-        }
-        else if( Math.floor(xClick - xMove) < -5 ){
-            $(".carousel").carousel('prev');
-        }
-    });
-    $(".carousel").on("touchend", function(){
-            $(this).off("touchmove");
-    });
-  });
+//  $(".this").on("touchstart", function(event){
+//        var xClick = event.originalEvent.touches[0].pageX;
+//    $(this).one("touchmove", function(event){
+//        var xMove = event.originalEvent.touches[0].pageX;
+//        if( Math.floor(xClick - xMove) > 5 ){
+//            $(".carousel").carousel('next');
+//        }
+//        else if( Math.floor(xClick - xMove) < -5 ){
+//            $(".carousel").carousel('prev');
+//        }
+//    });
+//    $(".carousel").on("touchend", function(){
+//            $(this).off("touchmove");
+//    });
+//  });
 })
