f01c4ea76d71a44bff80cb8565ed2ee64e380555
Sui Gao <Mark_sVi@outlook.com>
2017-08-31 22:56:12 -0700

    comment code

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,10 +1,12 @@
 $(document).ready(() => {
+  // only expand one card at a time
   let text = $('.text.collapse')
-  let toggle = $('.header a')
+  let toggle = $('a.header')
   let curIdx = 0
 
-  toggle.click(e => {
+  toggle.click(function () {
     text.eq(curIdx).collapse('hide')
-    curIdx = toggle.index(e.target)
+    curIdx = toggle.index(this)
   })
+  // end expand card logic
 })
