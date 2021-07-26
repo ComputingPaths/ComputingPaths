af9800399971c27174aa4a1cb7bb01fb05c87412
joey <joeydangg@gmail.com>
2021-07-25 23:28:16 -0700

    Add boilerplate

---
+++ new/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/reset.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    Computing Paths
+  </React.StrictMode>,
+  document.getElementById('root')
+);
