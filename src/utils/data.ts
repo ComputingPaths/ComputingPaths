524d696f11805fcd93a82fc13ae14b72733c8d08
joey <joeydangg@gmail.com>
2021-09-13 23:06:13 -0700

    Refactor content fetching

---
+++ src/utils/data.ts
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
+
+export const useData: React.FC = (page: string): Array<{}> => {
+  const [data, setData] = useState([]);
+
+  const contentMappings = {
+    pages: '1910980686',
+    stories: '1646384161',
+    majors: '860790804',
+    departments: '929723649',
+    majorDescriptions: '1664677404',
+    majorExpand: '1867072131',
+    resources: '2135229956',
+    resourcesTags: '167316779',
+    resourceBanner: '1661600010',
+    projects: '1379985722',
+    projectTags: '568500304',
+    orgs: '1399526925',
+    orgTags: '218206546',
+  };
+
+  useEffect(() => {
+    const gid = contentMappings[page];
+    Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vQPxfDC-DdscHUL8Zj8ObqyoyaB92ffcMtoWnFMbM1oZeCFG6Jwxba23ysjZ2JJEKpPdNwaKTj3PdH5/pub?output=csv&gid=${gid}`, {
+      download: true,
+      header: true,
+      complete: (results) => {
+        setData(results.data);
+      },
+    });
+  }, [page]);
+
+  return data;
+};
