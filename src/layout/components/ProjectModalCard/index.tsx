474fa356d9f98d931479d62eb5c42f73751c93b9
joey <joeydangg@gmail.com>
2021-09-29 00:54:21 -0700

    Add project modal card

---
+++ src/layout/components/ProjectModalCard/index.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+import LeftArrow from '../../../assets/LeftArrow.svg';
+import LinkedArrow from '../../../assets/LinkedArrow.svg';
+import RightArrow from '../../../assets/RightArrow.svg';
+
+import './style.scss';
+
+interface ProjectModalCardProps {
+    description: string;
+    organization: string;
+    photoURL: string;
+    projectMembers: string;
+    projectName: string;
+    projectTag: string;
+    projectLink?: string;
+    videoURL?: string;
+}
+
+const ProjectModalCard: React.FC<ProjectModalCardProps> = ({
+  description,
+  organization,
+  photoURL,
+  projectMembers,
+  projectName,
+  projectTag,
+  projectLink,
+  videoURL,
+}) => {
+  const [slide, setSlide] = useState(0);
+  return (
+    <div className="project-modal-card">
+      <section className="project-modal-card-carousel">
+        {photoURL.split(',').map((picture, index) => {
+          if (index === slide) {
+            return (
+              <img className="project-modal-card-image" src={picture} alt="project" />
+            );
+          }
+          return null;
+        })}
+        <button className="project-modal-card-arrow left" type="button" onClick={() => setSlide(slide - 1 === -1 ? photoURL.split(',').length - 1 : slide - 1)}><img className="project-modal-card-point" src={LeftArrow} alt="back arrow" /></button>
+        <button className="project-modal-card-arrow right" type="button" onClick={() => setSlide(slide + 1 === photoURL.split(',').length ? 0 : slide + 1)}><img className="project-modal-card-point" src={RightArrow} alt="right arrow" /></button>
+        <div className="project-modal-card-slide">
+          {photoURL.split(',').map((photo, index) => (
+            <div className={slide === index ? 'project-modal-card-dot select' : 'project-modal-card-dot'} />
+          ))}
+        </div>
+      </section>
+      <p className="project-modal-card-tag">
+        <span className="project-modal-card-project-name">{projectName}</span>
+        <span className="project-modal-card-organization">{organization}</span>
+        <span className={`project-modal-card-project-tag ${projectTag.toLowerCase()}`}>{projectTag}</span>
+      </p>
+      <div className="project-modal-card-section">
+        <p className="project-modal-card-description">
+          <span className="project-modal-card-header">Description</span>
+          {description.split('\\n\\n').map((paragraph) => (
+            <p className="project-modal-card-text">{paragraph}</p>
+          ))}
+        </p>
+        <section className="project-modal-card-info">
+          <ul className="project-modal-card-members">
+            <span className="project-modal-card-header">Project Members</span>
+            {projectMembers.split(',').map((name) => (
+              <li>{name}</li>
+            ))}
+          </ul>
+          <p className="project-modal-card-links">
+            <span className="project-modal-card-header">More Information</span>
+            <span>
+              <a className="project-modal-card-website" target="_blank" rel="noopener noreferrer" href={projectLink}> <span>Project Website</span> <img className="project-modal-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>
+              <a className="project-modal-card-website" target="_blank" rel="noopener noreferrer" href={videoURL}> <span>Youtube Link</span> <img className="project-modal-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>
+            </span>
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectModalCard;
