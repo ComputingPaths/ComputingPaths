c28aa4cfbadbbaf87c0dcccbe8b1c03dd4f6c7e6
Jennifer <32044772+Froggerson@users.noreply.github.com>
2021-09-28 23:59:25 -0700

    add basic majors page

---
+++ src/layout/components/MajorCard/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import LinkedArrow from '../../../assets/WhiteLinkedArrow.svg';
+
+import { DataTypes, useData } from '../../../utils/data';
+
+import './style.scss';
+
+interface MajorCardProps {
+    majorText?: string;
+    capped?: boolean;
+    degreeText?: string;
+    departments?: string[];
+    majorDescription?: string;
+    linkText?: string;
+    linkURL?: string;
+    photoURL?: string;
+}
+
+const MajorCard: React.FC<MajorCardProps> = ({
+  majorText,
+  capped,
+  degreeText,
+  departments,
+  majorDescription,
+  linkText,
+  linkURL,
+  photoURL,
+}) => {
+  const departmentData = useData(DataTypes.Departments);
+  const departmentURLs = new Map();
+
+  departmentData.forEach((department) => {
+    departmentURLs.set(department.Name, department.Link);
+  });
+
+  return (
+    <div className="major-card" id={majorText && majorText.replace(/\s/g, '-')}>
+      {photoURL && <img className="major-card-photo" src={photoURL} alt={`${majorText || 'Major Card'}`} />}
+      <div className="major-card-top">
+        {majorText && <p className="major-card-heading">{majorText}</p>}
+        {capped && <p className="major-card-tag">Capped</p>}
+        {degreeText && <p className="major-card-tag">{degreeText}</p>}
+      </div>
+      <div className="major-card-bottom">
+        <div className="major-card-bottom-left">
+          <p className="major-card-subheading">Description</p>
+          {majorDescription && <p className="major-card-description">{majorDescription}</p>}
+        </div>
+        <div className="major-card-bottom-right">
+          <p className="major-card-subheading">Departments</p>
+          <div className="major-card-links">
+            {departments && departments.map((department) => (
+              <a className="major-card-link" target="_blank" rel="noopener noreferrer" href={departmentURLs[department]}>{department}<img className="major-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>
+            ))}
+          </div>
+          <div className="major-card-links">
+            <p className="major-card-subheading">More Information</p>
+            <a style={{ textDecoration: 'underline' }} className="major-card-link" target="_blank" rel="noopener noreferrer" href={linkURL}>{linkText}<img className="major-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MajorCard;
--- src/layout/components/MajorPage/index.tsx
+++ src/layout/components/MajorPage/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import StickyBox from 'react-sticky-box';
+
+import MajorCard from '../MajorCard';
 
 import { DataTypes, useData } from '../../../utils/data';
 
@@ -6,11 +10,48 @@ import './style.scss';
 
 const MajorPage: React.FC = () => {
   const data = useData(DataTypes.Majors);
+  const departmentData = useData(DataTypes.Departments);
+  const departmentNames = {
+    bsc: 'Biological Sciences',
+    cse: 'Computer Science',
+    bie: 'Bioengineering',
+    cog: 'Cognitive Sciences',
+    mat: 'Mathematics',
+    phy: 'Physics',
+    vis: 'Visual Arts',
+    mus: 'Music',
+    ece: 'Electrical Engineering',
+  };
+  const departmentURLs = new Map();
+
+  departmentData.forEach((department) => {
+    departmentURLs.set(department.Name, department.Link);
+  });
 
   return (
     <div className="major-page">
-      Major
-      {data.map((major) => <h1>{major.Name}</h1>)}
+      <h1 className="major-page-title">Majors</h1>
+      <p className="major-page-text">A wide array of majors offer a unique computing experience at UCSD. These major apply computational skills to both new and traditional subject matter, developing programming skills, fostering scientific study, and bolstering creativity.</p>
+      <h2 className="major-page-subheading">Learn more about the computing majors</h2>
+      <div className="major-page-main-content">
+        <StickyBox className="major-page-sidebar" offsetTop={10} offsetBottom={10}>
+          {data.map((major) => <div className="major-page-link"><Link smooth to={`majors/#${major.Name.replace(/\s/g, '-')}`}>{major.Name}</Link></div>)}
+        </StickyBox>
+        <div className="major-page-cards">
+          {data.map((major) => (
+            <MajorCard
+              majorText={major.Name}
+              capped={major.Cap === 'TRUE'}
+              degreeText={major.Degree === 'bs' ? 'Bachelor of Science' : 'Bachelor of Arts'}
+              departments={major.Departments.split(',').map((element) => departmentNames[element])}
+              majorDescription={major.Hook}
+              photoURL={major.Image}
+              linkText={major['Moreinfo Name']}
+              linkURL={major['Moreinfo Link']}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
