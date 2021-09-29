fe4eae80caa8e42934af375cd16c56847768b698
joey <joeydangg@gmail.com>
2021-09-29 00:54:39 -0700

    Add project cards

---
+++ src/layout/components/ProjectCard/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import ProjectModalCard from '../ProjectModalCard';
+import ExpandArrow from '../../../assets/ExpandArrow.svg';
+
+import './style.scss';
+
+interface ProjectCardProps {
+    description: string;
+    organization: string;
+    photoURL: string;
+    projectLink: string;
+    projectMembers: string;
+    projectName: string;
+    projectTag: string;
+    videoURL: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  description,
+  organization,
+  photoURL,
+  projectLink,
+  projectMembers,
+  projectName,
+  projectTag,
+  videoURL,
+}) => {
+  const [modal, useModal] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      useModal(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
+
+  return (
+    <div ref={menuRef} className="project-card">
+      {photoURL && <img className="project-card-photo" src={photoURL.split(',')[0]} alt={`${projectName || 'Project Card'}`} />}
+      <p className="project-card-section">
+        <h2 className="project-card-heading">{projectName}</h2>
+        <h2 className="project-card-organization">{organization}</h2>
+        <span className={`project-card-project-tag ${projectTag.toLowerCase()}`}>{projectTag}</span>
+        <button className="project-card-expand-arrow" type="button" onClick={() => useModal(!modal)}>
+          <img src={ExpandArrow} alt="Expand Arrow" />
+        </button>
+      </p>
+      {modal && (
+      <ProjectModalCard
+        description={description}
+        organization={organization}
+        photoURL={photoURL}
+        projectMembers={projectMembers}
+        projectName={projectName}
+        projectTag={projectTag}
+        projectLink={projectLink}
+        videoURL={videoURL}
+      />
+      )}
+    </div>
+  );
+};
+
+export default ProjectCard;
