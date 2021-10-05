f2efd747e54ea9cd19f177a2d28b65f64225f456
Steven Steiner <steets250@gmail.com>
2021-10-04 22:08:33 -0700

    Add Issue Link to Footer

--- src/layout/components/Footer/index.tsx
+++ src/layout/components/Footer/index.tsx
@@ -15,6 +15,10 @@ const Footer: React.FC = () => (
     <div className="footer-links">
       {pages.map((page, index) => (<Link to={page.link}><p className="footer-link" key={index}>{page.title}</p></Link>))}
     </div>
+    <div className="footer-issue">
+      <p className="footer-issue-heading">Have an issue?</p>
+      <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/xKkMgde4E9p8Pmoo6"><p className="footer-issue-link">Fill out this form</p></a>
+    </div>
   </div>
 );
 
