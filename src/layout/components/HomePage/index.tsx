2fc9af52a8862dc84dfbedfa80e871bb3697f653
joey <joeydangg@gmail.com>
2021-11-11 20:39:00 -0800

    Update home page

--- src/layout/components/HomePage/index.tsx
+++ src/layout/components/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import CarouselRightArrow from '../../../assets/CarouselRightArrow.svg';
@@ -10,12 +10,23 @@ import { DataTypes, useData } from '../../../utils/data';
 import './style.scss';
 
 const HomePage: React.FC = () => {
-  const [data, setData] = useState<Array<any>>([]);
+  const [majors, setMajor] = useState<Array<any>>([]);
+  const [homeData, setHomeData] = useState<Array<any>>([]);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const handleNav = () => {
+    if (menuRef && menuRef.current) {
+      menuRef.current.scrollLeft += 450;
+    }
+  };
 
   useEffect(() => {
     useData(DataTypes.Majors)
-      .then((newData) => setData(newData))
-      .catch(() => setData([]));
+      .then((newData) => setMajor(newData))
+      .catch(() => setMajor([]));
+    useData(DataTypes.Home)
+      .then((newData) => setHomeData(newData))
+      .catch(() => setHomeData([]));
   }, [useData]);
 
   return (
@@ -27,31 +38,33 @@ const HomePage: React.FC = () => {
             <button className="home-page-home-button" type="submit">Find Your Home</button>
           </Link>
         </div>
-        <div className="home-page-image" />
+        <img className="home-page-image" src="/img/HomePageLogo.svg" alt="home page logo" />
       </section>
       <h2 className="home-page-header major">Majors</h2>
-      <section className="home-page-majors-container">
-        {data.map((major) => (
+      <section className="home-page-majors-container" ref={menuRef}>
+        {majors.map((major) => (
           <div className="home-page-majors-section">
-            <img className="home-page-majors-section-image" src={major.Image} alt="major" />
-            <h3 className="home-page-majors-section-major">{major.Name}</h3>
+            <img className="home-page-majors-section-image" src={major.image} alt="major" />
+            <h3 className="home-page-majors-section-major">{major.name}</h3>
           </div>
         ))}
       </section>
-      <img className="home-page-right-arrow" src={CarouselRightArrow} alt="Right Arrow" />
+      <button className="home-page-right-arrow" type="submit" onClick={handleNav}>
+        <img src={CarouselRightArrow} alt="Right Arrow" />
+      </button>
       <article className="home-page-stories-section">
         <h2 className="home-page-header">Stories &#38; Advice</h2>
         <section className="home-page-stories">
           <div className="home-page-stories-image">
             <img className="home-page-stories-gear" src={Gear} alt="Gear Quote" />
-            <img className="home-page-image-circle" src="/img/story.jpg" alt="Advice" />
+            <img className="home-page-image-circle" src={homeData.length > 0 ? homeData[0].story_photo : null} alt="Advice" />
           </div>
           <div className="home-page-stories-text">
             <img className="home-page-stories-left-quote" src={LeftQuote} alt="Left Quote" />
-            <p className="home-page-stories-quote">What sealed the deal on software engineering for me was that rush you get when something finally works</p>
+            <p className="home-page-stories-quote">{homeData.length > 0 ? homeData[0].story_quote : null}</p>
             <h3 className="home-page-stories-name">
-              Rachel Keirouz &nbsp;
-              <span>Mathematics - Computer Science</span>
+              {homeData.length > 0 ? homeData[0].story_name : null} &nbsp;
+              <span>{homeData.length > 0 ? homeData[0].study_bio : null}</span>
             </h3>
             <img className="home-page-stories-right-quote" src={RightQuote} alt="Right Quote" />
           </div>
@@ -59,7 +72,7 @@ const HomePage: React.FC = () => {
       </article>
       <h2 className="home-page-header">Get Involved</h2>
       <section className="home-page-resources">
-        <div className="home-page-image" />
+        <img className="home-page-involed-image" src={homeData.length > 0 ? homeData[0].student_org_photo : null} alt="org logo" />
         <div className="home-page-resources-section">
           <h2 className="home-page-subheader">Student Organizations</h2>
           <p className="home-page-text">Student Organizations allow for extracurricular expierence, utilizing and extending skills imparted in computing courses. These groups demonstrate creating computing efforts by channeling the collaborative spirit of UC San Diego.</p>
@@ -69,7 +82,7 @@ const HomePage: React.FC = () => {
         </div>
       </section>
       <section className="home-page-resources">
-        <div className="home-page-image" />
+        <img className="home-page-involed-image" src={homeData.length > 0 ? homeData[0].projects_photo : null} alt="projects logo" />
         <div className="home-page-resources-section">
           <h2 className="home-page-subheader">Projects</h2>
           <p className="home-page-text">Computing students create impressive bodies of work throughout their time at UC San Diego, whether for classes, internships, or just for fun.</p>
