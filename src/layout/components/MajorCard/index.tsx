4e139b21e72b64ae94b1ff3dd35fc34a76c92ee8
Steven Steiner <steets250@gmail.com>
2021-12-03 17:11:36 -0800

    Update Error + Card Styling

--- src/layout/components/MajorCard/index.tsx
+++ src/layout/components/MajorCard/index.tsx
@@ -35,43 +35,41 @@ const MajorCard: React.FC<MajorCardProps> = ({
           </div>
           )}
         </div>
-        <div className="major-card-middle">
-          <div className="major-card-middle-left">
-            <p className="major-card-subheading">Description</p>
-            {description && <p className="major-card-description">{description}</p>}
-          </div>
-          <div className="major-card-middle-right">
-            <p className="major-card-subheading">Departments</p>
-            <div className="major-card-links">
-              {departments && departments.map((department, index) => department.title && department.url && <a className="major-card-link" target="_blank" rel="noopener noreferrer" href={department.url} key={index}>{department.title}<img className="major-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>)}
+        <div className="major-card-bottom">
+          <div className="major-card-info">
+            <div className="major-card-info-left">
+              <p className="major-card-subheading">Description</p>
+              {description && <p className="major-card-description">{description}</p>}
             </div>
-            <div className="major-card-links">
-              <p className="major-card-subheading">More Information</p>
-              {links && links.map((link, index) => link.title && link.url && <a className="major-card-link" target="_blank" rel="noopener noreferrer" href={link.url} key={index}>{link.title}<img className="major-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>)}
+            <div className="major-card-info-right">
+              <p className="major-card-subheading">Departments</p>
+              <div className="major-card-links">
+                {departments && departments.map((department, index) => department.title && department.url && <a className="major-card-link" target="_blank" rel="noopener noreferrer" href={department.url} key={index}>{department.title}<img className="major-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>)}
+              </div>
+              <div className="major-card-links">
+                <p className="major-card-subheading">More Information</p>
+                {links && links.map((link, index) => link.title && link.url && <a className="major-card-link" target="_blank" rel="noopener noreferrer" href={link.url} key={index}>{link.title}<img className="major-card-link-arrow" src={LinkedArrow} alt="Link Arrow" /></a>)}
+              </div>
             </div>
           </div>
+          {specializations.length !== 0 && (specializations.map((specialization, index) => (
+            <div className="major-card-specialization" key={index}>
+              <div className="major-card-specialization-heading">
+                <p className="major-card-specialization-name">{specialization.name}</p>
+                <button className={`major-card-specialization-button ${open === index ? 'open' : ''}`} type="button" onClick={() => setOpen(open === index ? -1 : index)}><img src={Caret} alt="Description" /></button>
+              </div>
+              <div className={`major-card-specialization-content ${open === index ? 'open' : ''}`}>
+                <p className="major-card-specialization-detail">{specialization.detail}</p>
+              </div>
+            </div>
+          ))
+          )}
+          {note && (
+            <div className="major-card-note">
+              <p className="major-card-note-text">{note}</p>
+            </div>
+          )}
         </div>
-        {specializations.length !== 0
-      && (
-      <div className="major-card-bottom">
-        {specializations.map((specialization, index) => (
-          <div className="major-card-specialization" key={index}>
-            <div className="major-card-specialization-heading">
-              <p className="major-card-specialization-name">{specialization.name}</p>
-              <button className={`major-card-specialization-button ${open === index ? 'open' : ''}`} type="button" onClick={() => setOpen(open === index ? -1 : index)}><img src={Caret} alt="Description" /></button>
-            </div>
-            <div className={`major-card-specialization-content ${open === index ? 'open' : ''}`}>
-              <p className="major-card-specialization-detail">{specialization.detail}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-      )}
-        {note && (
-        <div className="major-card-note">
-          <p className="major-card-note-text">{note}</p>
-        </div>
-        )}
       </div>
     </>
   );
