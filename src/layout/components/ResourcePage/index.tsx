720114145e800a8568d3ad8ba06fe35974774805
TylerLo416 <93793209+TylerLo416@users.noreply.github.com>
2022-07-24 01:47:23 -0700

    quick bugfix
    heading was in the incorrect place

--- src/layout/components/ResourcePage/index.tsx
+++ src/layout/components/ResourcePage/index.tsx
@@ -40,6 +40,7 @@ const ResourcePage: React.FC = () => {
       <h1 className="resource-page-title">Resources</h1>
       <p className="resource-page-text">Discover the many resources available at UC San Diego: study spaces, career and research opportunities, and academic support.</p>
       <div className="resource-page-content">
+      <p className="resource-page-heading">Become familiar with campus resources</p>
         <div className="resource-page-map">
           <iframe
             title="resource-map"
@@ -48,7 +49,6 @@ const ResourcePage: React.FC = () => {
             height="960"
           />
         </div>
-        <p className="resource-page-heading">Become familiar with campus resources</p>
         <div className="resource-page-tag-section">
           <button className={`resource-page-tag-button${filter === '' ? ' selected' : ''}`} type="button" onClick={() => setFilter('')}>All</button>
           {
